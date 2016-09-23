01d4354291144b02f720d1315f1ea8a44bd0bdd6
Augusto <augusto@Augustos-MBP.attlocal.net>
2016-09-22 19:29:03 -0700

    Second Commit

---
+++ TrainerApp/TrainerApp/Evaluation.swift
@@ -0,0 +1,9 @@
+//
+//  Evaluation.swift
+//  TrainerApp
+//
+//  Created by Augusto on 9/22/16.
+//  Copyright © 2016 Aggy Development Ltda. All rights reserved.
+//
+
+import Foundation
---
+++ TrainerApp/TrainerApp/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  TrainerApp
+//
+//  Created by Augusto on 9/22/16.
+//  Copyright © 2016 Aggy Development Ltda. All rights reserved.
+//
+
+import Foundation
